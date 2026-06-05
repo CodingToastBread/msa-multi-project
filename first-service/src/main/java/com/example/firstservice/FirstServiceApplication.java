6ff05b49b2a2614c4a74077301635579087ad4c4
CodingToastBread <mickeymouset@naver.com>
2026-06-05 13:03:54 +0900

    add first,second service

--- first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.gateway;
+package com.example.firstservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class FirstServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(FirstServiceApplication.class, args);
     }
 
 }
