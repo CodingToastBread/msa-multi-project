466c727068d25d9b4a9fce302ab4a7b5b7e99cb4
CodingToastBread <mickeymouset@naver.com>
2026-09-06 13:24:20 +0900

    [FILE] Httpie 요청 export json 생성

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -4,4 +4,3 @@ insert into catalog(product_id, product_name, stock, unit_price)
 values ('CATALOG-002', 'Tokyo', 100, 900);
 insert into catalog(product_id, product_name, stock, unit_price)
 values ('CATALOG-003', 'Stockholm', 100, 1200);
-리
