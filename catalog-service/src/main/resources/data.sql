8eacdcc3953babf373db9e74e721a227019fb74e
CodingToastBread <mickeymouset@naver.com>
2026-09-06 13:22:38 +0900

    [REFACT] 카프카 OrderService 구매 -> Catalog 총수량 뺄셈 처리(2)

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -4,3 +4,4 @@ insert into catalog(product_id, product_name, stock, unit_price)
 values ('CATALOG-002', 'Tokyo', 100, 900);
 insert into catalog(product_id, product_name, stock, unit_price)
 values ('CATALOG-003', 'Stockholm', 100, 1200);
+리
